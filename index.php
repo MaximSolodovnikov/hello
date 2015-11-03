3c90429f6025d7086b7e467d00f38c52e90c132a
MaximSolodovnikov <MaximSolodovnikov@yandex.ua>
2015-11-03 07:54:04 +0200

    Beginning

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>jQuery</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	</head>
+	<body>
+		<h2>Теперь пора приступить к работе.</h2>
+	</body>
+</html>
+
